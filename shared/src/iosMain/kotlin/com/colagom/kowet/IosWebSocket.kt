3defec8df879461f84c33f6eec331d4a83ecc7ad
ColaGom <bchoi000@gmail.com>
2022-03-19 17:39:13 +0900

    feat: implements WebSocketImpl for ios

--- shared/src/iosMain/kotlin/com/colagom/kowet/IosWebSocket.kt
+++ shared/src/iosMain/kotlin/com/colagom/kowet/IosWebSocket.kt
@@ -1,22 +1,80 @@
 package com.colagom.kowet
 
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.callbackFlow
+import platform.Foundation.*
+import platform.darwin.NSObject
 
+actual class WebSocketImpl(
+    private val url: String
+) : WebSocket {
+    private var realSocket: NSURLSessionWebSocketTask? = null
 
-actual class WebSocketImpl : WebSocket {
     override fun open(): Flow<WebSocket.Event> {
-        TODO("Not yet implemented")
+        return callbackFlow {
+            val urlSession = NSURLSession.sessionWithConfiguration(
+                configuration = NSURLSessionConfiguration.defaultSessionConfiguration(),
+                delegate = object : NSObject(), NSURLSessionWebSocketDelegateProtocol {
+                    override fun URLSession(
+                        session: NSURLSession,
+                        webSocketTask: NSURLSessionWebSocketTask,
+                        didOpenWithProtocol: String?
+                    ) {
+                        trySend(WebSocket.Event.OnOpen)
+                    }
+
+                    override fun URLSession(
+                        session: NSURLSession,
+                        webSocketTask: NSURLSessionWebSocketTask,
+                        didCloseWithCode: NSURLSessionWebSocketCloseCode,
+                        reason: NSData?
+                    ) {
+                        trySend(
+                            WebSocket.Event.OnClosed(
+                                ShutdownReason(
+                                    didCloseWithCode.toInt(),
+                                    reason.toString()
+                                )
+                            )
+                        )
+                    }
+                },
+                delegateQueue = NSOperationQueue.currentQueue()
+            )
+
+            realSocket = urlSession.webSocketTaskWithURL(NSURL.URLWithString(url)!!)
+            realSocket?.receiveMessageWithCompletionHandler { message, nsError ->
+                when {
+                    nsError != null -> {
+                        trySend(WebSocket.Event.OnFailure(Throwable(nsError.description)))
+                    }
+                    message != null -> {
+                        message.string?.let {
+                            trySend(WebSocket.Event.OnMessage(it))
+                        }
+                    }
+                }
+            }
+            realSocket?.resume()
+        }
     }
 
     override fun send(message: String): Boolean {
-        TODO("Not yet implemented")
+        realSocket?.sendMessage(NSURLSessionWebSocketMessage(message)) {
+            /** TODO : Logging **/
+        }
+
+        return true
     }
 
-    override fun close() {
-        TODO("Not yet implemented")
+    override fun close(): Boolean {
+        val res = realSocket != null
+        realSocket?.cancelWithCloseCode(1000, null)
+        realSocket = null
+        return res
     }
 }
 
 actual object WebSocketFactory : WebSocket.Factory {
-    override fun create(): WebSocket = TODO()
+    override fun create(url: String): WebSocket = WebSocketImpl(url)
 }
